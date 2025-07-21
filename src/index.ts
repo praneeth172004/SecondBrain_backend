f3239cb4a9a209ca2326377651578c42e2e8fb8a
praneeth172004 <sunnypraneeth3119@gmail.com>
2025-07-21 10:50:02 +0530

    token

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,9 @@ app.use(cors({
   origin: [ "https://second-brain-frontend-zruo.vercel.app",
 "http://localhost:5173"],
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   exposedHeaders: ['Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }))
 app.post('/user/signup', async (req: Request, res: Response):Promise<any> => {
   try {
