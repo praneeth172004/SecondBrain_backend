4e869c43cfca8257e117e885212f047921f2bfad
praneeth172004 <sunnypraneeth3119@gmail.com>
2025-07-21 11:51:08 +0530

    authCorrections dotenv

--- src/Middleware/Authmiddleware.ts
+++ src/Middleware/Authmiddleware.ts
@@ -1,5 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import jwt, { JwtPayload } from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 // Extend Express Request to include userId
 
